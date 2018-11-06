a601dce8c5e58dd7194140b61acb552714f8f537
Stuart Terrett <stuart.terrett@oracle.com>
2018-11-06 11:39:52 -0500

    Introduction

--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -25,7 +25,9 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+  val msg = message ?: return
+  val email = client?.personalInfo?.email ?: return
+  mailer.sendMessage(email, message);
 }
 
 class Client (val personalInfo: PersonalInfo?)
